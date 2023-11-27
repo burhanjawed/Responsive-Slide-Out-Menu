76f0fd789e0efe57344db1c82681f2a7d3ea4f92
Burhan Jawed <71589999+tibsxi@users.noreply.github.com>
2023-11-27 12:01:33 -0500

    Added javascript and gsap animations

---
+++ .history/script_20231127115103.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.querySelector('.container');
+  const sidebar = document.querySelector('.sidebar');
+  const cards = gsap.utils.toArray('.card');
+  console.log('Script is working!');
+});
